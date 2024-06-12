30d72d8d2b49acc24e673c4739239d0ac4e75ce5
Tanjil <tanjilkh@gmail.com>
2024-06-12 15:35:05 +0100

    criar colecao

--- script.py
+++ script.py
@@ -5,6 +5,13 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 # Criar base de dados "stand_auto"
 stand_auto = client["stand_auto"]
 
+# Verifica se já existe a base de dados
 dblist = client.list_database_names()
 if "stand_auto" in dblist:
     print("A base de dados já existe.")
+
+collecaoCarros = stand_auto["carros"]
+
+collist = stand_auto.list_collection_names()
+if "carros" in collist:
+    print("A coleção carros já existe")
